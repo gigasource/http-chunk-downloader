ae6356ccc566f1d1e0f9dc3628b8223d90a80a97
ThinhVu <vutrongthinhk7@gmail.com>
2020-07-09 16:12:43 +0700

    Download chunk into file via file-stream instead of storing data in memory Support download entire file method.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const axios = require('axios');
-const md5 = require('md5');
+const axios = require('axios')
+const md5 = require('md5')
+const fs = require('fs')
+const tmp = require('tmp-promise')
 
 /**
  * https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Range
@@ -7,7 +9,7 @@ const md5 = require('md5');
  * @param {String} src.url: CDN url
  * @param {String} src.checksumUrl: checkSum Url
  * @param {Object} config
- * @param {Boolean} [config.skipCheck=true]: Skip checksum checking step
+ * @param {Boolean} [config.skipCheck]: Skip checksum checking step
  * @param {Number} [config.range.start=0]: start offset
  * @param {Number} [config.range.end]: end offset
  * @param {Number} [config.range.length]: Number of unit will be received, only used if range.end is missing
@@ -16,7 +18,7 @@ const md5 = require('md5');
  * @param {Function} [config.generateChecksum=md5]: Custom generate checksum on received data. If not provided, default 'md5' will be used
  * @returns {Promise<null|*>}
  */
-async function download(src, config) {
+async function downloadChunk(src, config) {
   let url
   let checksumUrl
 
@@ -42,7 +44,6 @@ async function download(src, config) {
       headers: { 'range': `bytes=${start}-${end}` }
     };
     checksumCfg = {
-      responseType: 'arraybuffer',
       headers: { 'range': `bytes=${start}-${end}`, 'check_sum': 1, }
     }
   }
@@ -52,7 +53,7 @@ async function download(src, config) {
   if (!skipCheck) {
     for(let i=0;i<retry;++i) {
       try {
-        checkSum = (await axios.get(checksumUrl, checksumCfg)).data.toString()
+        checkSum = (await axios.get(checksumUrl, checksumCfg)).data
         break
       } catch (e) {
         onError && onError(e, i)
@@ -67,14 +68,15 @@ async function download(src, config) {
   let ourCheckSum
   for(let i=0;i<retry;++i) {
     try {
-      data = (await axios.get(url, cfg)).data
-
+      const filePath = await _downloadToFile(url, cfg)
       if (skipCheck)
-        return data
-
-      ourCheckSum = generateChecksum(data)
+        return filePath
+      const readStream = fs.createReadStream(filePath)
+      const data2 = await _stream2Buffer(readStream)
+      ourCheckSum = generateChecksum(data2)
+      readStream.close()
       if (checkSum === ourCheckSum) {
-        return data
+        return filePath
       } else {
         onError && onError({
           message: `Checksum mismatch. Expect: ${checkSum}, Received: ${ourCheckSum}`
@@ -88,24 +90,83 @@ async function download(src, config) {
   throw "Download failed"
 }
 
-// download entire an image, no cdn
-// const image = await download('https://ggs.io/background.png')
+/**
+ * @param {String | Object} src download url
+ * @param {String} src.url: CDN url
+ * @param {String} src.checksumUrl: checkSum Url
+ * @param config
+ * @param {Boolean} [config.skipCheck]: Skip checksum checking step
+ * @param {Number} [config.retry=1]: Number the number of time try to re-download if failed
+ * @param {Function} [config.onError]: Error handle function to handle error e at download number i
+ * @param {Function} [config.generateChecksum=md5]: Custom generate checksum on received data. If not provided, default 'md5' will be used
+ * @returns {Promise<void>}
+ */
+async function download(src, config) {
+  let chunkSizeInBytes = config.chunkSizeInBytes || 1024 * 1024 // 1 MB
+  if (chunkSizeInBytes <= 0)
+    throw `Invalid chunk size: ${chunkSizeInBytes}`
 
-// download entire an image with cdn
-// const image = await download({
-//    url: 'https://cdn.ggs.io/background.png',
-//    checksumUrl: 'https://ggs.io/background.png'
-// })
+  console.log('Downloading chunks...')
+  const filePaths = []
+  let start = 0
+  while(true) {
+    const filePath = await downloadChunk(src, { ...config, range: { start, length: chunkSizeInBytes } })
+    filePaths.push(filePath)
+    const fileStat = fs.statSync(filePath)
+    if (fileStat.size !== chunkSizeInBytes)
+      break;
+    start += chunkSizeInBytes
+  }
 
-// download with options
-// const video = await download({
-//    url: 'https://cdn.ggs.io/intro.mp4',
-//    checksumUrl: 'https://ggs.io/intro.mp4'
-// }, {
-//   retry: 10,
-//   range: { length: 1024 * 1024 }, /*first 1MB, similar too { end: 1024 * 1024 - 1 } or { start: 0, length: 1024 * 1024 }  or { start: 0, end: 1024 * 1024 - 1 }  */
-//   onError: (e, i) => console.log(`Error ${e} at try ${i}`), // error handle at i failed
-//   generateChecksum: data => md5(UInt8Array(data)) // custom md5 checksum, in-case data need to modify before generate hash
-// })
+  console.log('Joining chunks...')
+  const outputFile = tmp.tmpNameSync()
+  for(let filePath of filePaths) {
+    await _appendFile(filePath, outputFile)
+  }
 
-module.exports = download
+  // const stat = fs.statSync(outputFile)
+  // console.log(stat)
+  // const outputHash = await md5(await _stream2Buffer(fs.createReadStream(outputFile)))
+  // const serverHash = (await axios.get(src, { headers: { 'check_sum': 1 } })).data
+  // console.log(outputHash)
+  // console.log(serverHash)
+
+  return outputFile
+}
+
+function _appendFile(fsrc, fdest) {
+  return new Promise((resolve, reject) => {
+    const f1 = fs.createReadStream(fsrc)
+    const f2 = fs.createWriteStream(fdest, { flags: 'a' })
+    f2.on('error', reject)
+    f2.on('finish', resolve)
+    f1.pipe(f2)
+  })
+}
+function _stream2Buffer(stream) {
+  return new Promise((resolve) => {
+    const bufs = [];
+    stream.on('data', d => bufs.push(d))
+    stream.on('end', () => {
+      resolve(Buffer.concat(bufs))
+    })
+  })
+}
+async function _downloadToFile(url, config) {
+  const filePath = tmp.tmpNameSync()
+  const writer = fs.createWriteStream(filePath)
+  config.responseType = 'stream'
+  const response = await axios.get(url, config)
+  response.data.pipe(writer)
+  return new Promise((resolve, reject) => {
+    writer.on('finish', () => {
+      resolve(filePath)
+    })
+    writer.on('error', reject)
+  })
+}
+
+module.exports = {
+  download,
+  downloadChunk
+}
--- src/test.js
+++ src/test.js
@@ -1,8 +1,26 @@
-const download = require('./index')
+(async () => {
+  const { download } = require('./index')
+  const data = await download('http://localhost/Steve1.mp4', { retry: 10, onError: (e, i) => {
+      console.log(e, i)
+    } })
+  console.log(data)
+})()
 
-const data = download('http://localhost/bentley-1.jpg', { range: { length: 100 }, retry: 10, onError: (e, i) => {
-    console.log(e)
-  }
-})
 
-console.log(data)
+// download entire an image, no cdn
+// const image = await download('https://ggs.io/background.png')
+// download entire an image with cdn
+// const image = await download({
+//    url: 'https://cdn.ggs.io/background.png',
+//    checksumUrl: 'https://ggs.io/background.png'
+// })
+// download with options
+// const video = await download({
+//    url: 'https://cdn.ggs.io/intro.mp4',
+//    checksumUrl: 'https://ggs.io/intro.mp4'
+// }, {
+//   retry: 10,
+//   range: { length: 1024 * 1024 }, /*first 1MB, similar too { end: 1024 * 1024 - 1 } or { start: 0, length: 1024 * 1024 }  or { start: 0, end: 1024 * 1024 - 1 }  */
+//   onError: (e, i) => console.log(`Error ${e} at try ${i}`), // error handle at i failed
+//   generateChecksum: data => md5(UInt8Array(data)) // custom md5 checksum, in-case data need to modify before generate hash
+// })
